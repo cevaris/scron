6359f9d527abcc4b4d8cbd1f8c2c67e3aa949fad
Cevaris <cevaris@gmail.com>
2014-06-14 20:45:04 -0600

    ran into max thread count errors

--- scron.py
+++ scron.py
@@ -4,22 +4,41 @@
 import os
 import threading
 import datetime
+import signal
+import sys
+from subprocess import Popen
 
 BASE_PATH   = '/Users/cevaris/Documents/workspace/scron'
 WORKER_PATH = os.path.join(BASE_PATH, 'worker.py')
 
+DELTAS = []
 
-def do_work_repeat(*args, **kwargs):
-    print args, kwargs
+def stats():
+    print 'Average Delta: ', reduce(lambda x, y: x + y, DELTAS) / len(DELTAS)
+
+def signal_handler(signal, frame):
+    stats()
+    sys.exit(0)
 
 
 def do_work(*args, **kwargs):
-    repeat = kwargs.pop('repeat')
-    time   = kwargs.pop('time')
+    repeat   = kwargs.pop('repeat')
+    time     = kwargs.pop('time')
+    started  = kwargs.pop('started')
+    executed = datetime.datetime.now()
+    delta    = executed - started
+    DELTAS.append(delta)
+
+    print "Executed worker scheduled at %d in %s" % (time, str(delta))
+
     if repeat:
-        threading.Timer(time, do_work, [args[0], args[1]], {'repeat': repeat, 'time':time}).start()
-    
+        schedule(time, args[0], args[1], repeat)
+
+    # Execute actual work
     os.system("%s %s" % (args[0], args[1]))
+
+    threading.exit()
+
     
 
 def schedule(time, exec_path, params=[], repeat=False):
@@ -29,27 +48,28 @@ def schedule(time, exec_path, params=[], repeat=False):
         if not (exec_path and os.path.exists(exec_path)):
             raise Exception('Execution path must point to a valid/existing file',exec_path)
 
-
     if validate:
-        print 'Valid schedule item'
-        timer = threading.Timer(time, do_work, [exec_path, params], {'repeat': repeat, 'time':time})
+        # print 'Valid schedule item'
+        started = datetime.datetime.now()
+        timer = threading.Timer(time, do_work, [exec_path, params], {'repeat': repeat, 'time':time, 'started': started})
         timer.start()
-
+        return True
+    else:
+        return False       
 
 
 
 def main():
 
+    for x in range(1,100):
+        print x, schedule(3, WORKER_PATH, ['echo', 'news', 'welcome'], False)
 
-    
-    # schedule(10, WORKER_PATH, {}, False)
-    # schedule(10, 'fake path', {}, False)
-    # schedule('10s', WORKER_PATH, {}, False)
-    # schedule('10', None, {}, False)
+    signal.signal(signal.SIGINT, signal_handler)
+    print('Press Ctrl+C')
+    signal.pause()
 
-    print schedule(3, WORKER_PATH, ['echo', 'news', 'welcome'], True)
-    # print schedule(1, WORKER_PATH, ['worker', 'does', 'nddds'], True)
-    
+
+    stats()
     print 'Done'
 
 
--- worker.py
+++ worker.py
@@ -2,4 +2,7 @@
 
 import sys
 
+for l in range(1,1000):
+    for l in range(1,1000):
+        pass
 print 'test', str(sys.argv)
