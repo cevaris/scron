fa374eeab0ce7bc919110bc0143e60fd062a949b
Cevaris <cevaris@gmail.com>
2014-06-16 23:19:30 -0600

    cleaned up code

--- scron-api/config/routes.rb
+++ scron-api/config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   constraints subdomain: 'api' do
     scope module: 'api' do
       namespace :v1 do
-
         resources :events, defaults: {format: :json}
-
       end
     end
   end
